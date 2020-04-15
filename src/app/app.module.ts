417b121406549a30968c9fdc5c443028e21f3358
manimbbm <marqueisbruno@gmail.com>
2020-04-15 07:46:41 +0100

    Added hardcoded fun tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { ModalStructureComponent } from './modal-structure/modal-structure.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatIconModule, MatMenuModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatIconModule, MatMenuModule} from '@angular/material';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { FunComponent } from './fun/fun.component';
@@ -32,7 +32,8 @@ import { SettingsComponent } from './settings/settings.component';
     MatMenuModule,
     MatButtonModule,
     MatIconModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/fun/fun.component.ts
+++ src/app/fun/fun.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-fun',
@@ -6,6 +7,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fun.component.css']
 })
 export class FunComponent implements OnInit {
+  tasks = [
+    {
+      task: 'Cook',
+      skill: 2,
+      affection: 5
+    },
+    {
+      task: 'Read',
+      skill: 2,
+      affection: 5
+    },
+    {
+      task: 'Watch',
+      skill: 2,
+      affection: 5
+    },
+    {
+      task: 'Build',
+      skill: 2,
+      affection: 5
+    },
+    {
+      task: 'Play Games',
+      skill: 2,
+      affection: 5
+    },
+    {
+      task: 'Play Instruments',
+      skill: 2,
+      affection: 5
+    },
+    {
+      task: 'Share posts',
+      skill: 2,
+      affection: 5
+    },
+    {
+      task: 'task2',
+      skill: 2,
+      affection: 5
+    },
+    {
+      task: 'task2',
+      skill: 2,
+      affection: 5
+    },
+    {
+      task: 'task2',
+      skill: 2,
+      affection: 5
+    },
+    {
+      task: 'task2',
+      skill: 2,
+      affection: 5
+    },
+    {
+      task: 'task2',
+      skill: 2,
+      affection: 5
+    },
+  ];
 
   constructor() { }
 
